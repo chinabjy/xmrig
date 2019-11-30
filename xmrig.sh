01bd95d92ab49ffcef249e12f5414ab47d56c25f
chinabjy <48501377+chinabjy@users.noreply.github.com>
2019-11-30 20:10:45 +0800

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-screen -S xmrig;
-ping www.baidu.com;
+dd if=/dev/zero of=/swapfile bs=4096 count=1024K;
+mkswap /swapfile;
+swapon /swapfile;
+swapon -s;
+sudo yum install -y epel-release;
+sudo yum install -y git make cmake gcc gcc-c++ libstdc++-static libuv-static hwloc-devel openssl-devel;
+git clone https://github.com/xmrig/xmrig.git;
+cd xmrig && mkdir build && cd build
+cmake .. -DUV_LIBRARY=/usr/lib64/libuv.a;
+make;
+./xmrig --donate-level 1 --http-enabled --api-worker-id usa --http-host 0.0.0.0 --http-port 11555 --http-access-token chinabjy --http-no-restricted -o pool.hashvault.pro:443 -u 41xYQJ9xT1jPbyyEUa73trX1HQX22UVKBFsiAJqo2HgDHq9u8zzbpsh3GANLgUCLvThQff514HFUdbWjuYjY4ZYUKZHFuwo -p usa2 -k --tls
